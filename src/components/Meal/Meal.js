c1348a949fb4e749f2895a92f7c27da3cff8e10e
abujakariacse <abujakaria316@gmail.com>
2022-03-25 18:26:00 +0600

    card and button added

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Meal.css'
 
 const Meal = (props) => {
-    console.log(props.meal);
     const {strMealThumb,strMeal,strCategory,strArea,strInstructions} = props.meal;
     return (
         <div className='meal'>
             <img className='foodimg' src={strMealThumb} alt="" />
             <h4>Name: {strMeal}</h4>
-            <p>Category: {strCategory}</p>
-            <p>Manufacturer: {strArea}</p>
+            <p className='subtext'>Category: {strCategory}</p>
+            <p className='subtext'>Manufacturer: {strArea}</p>
             <p>{strInstructions.slice(4,100)}</p>
+            <button>Add to Cart</button>
         </div>
     );
 };
